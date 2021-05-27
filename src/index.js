138987a057f71e4f4083964494dfbf822e9b6dd6
Alanisia <2478196682@qq.com>
2021-05-27 23:45:02 +0800

    [UPDATE] add icons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/**
+ * @author 3118004765
+ */ 
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
@@ -5,8 +8,10 @@ import * as Keys from './components/city/city-common';
 import './components/App.css';
 
 let list = localStorage.getItem(Keys.default.currentListKey);
-if (list === undefined || list === null || list === [] || list === "")
+if (list === undefined || list === null || list === [] || list === "") {
   localStorage.setItem(Keys.default.currentListKey, JSON.stringify([]));
+  alert("请添加城市！");
+}
 
 let caches = localStorage.getItem(Keys.default.cityCache);
 if (caches === undefined || caches === null || caches === [] || caches === "")
